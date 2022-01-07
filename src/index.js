3c39a7b1deda58994f6d52bea4c891a03d9fa0bb
Mathilde <nihaomouw@mac.com>
2022-01-06 19:37:20 -0800

    populate solved sudoku puzzle

--- src/index.js
+++ src/index.js
@@ -49,22 +49,122 @@ class Board extends React.Component {
 	}
 
 	initialSquares() {
-		let squares =
-			[{},{},{},{},
+		let squares = [
 			{},{},{},{},
 			{},{},{},{},
-			{},{},{},{}]
+			{},{},{},{},
+			{},{},{},{}
+		]
 
 		let fillOptions = [1,2,3,4]
 
 		for(let i=0; i<16; i++){
-			if (fillOptions.length == 0) {
-				fillOptions = [1,2,3,4]
-			}
-			squares[i].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)
+			// if (fillOptions.length == 0) {
+			// 	fillOptions = [1,2,3,4]
+			// }
+			// squares[i].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)
+
+			squares[i].value = i
 		}
 
+		this.solveFirstQuadrant(squares);
+		this.solveSecondQuadrant(squares);
+		this.solveThirdQuadrant(squares);
+		this.solveFourthQuadrant(squares);
+
 		return squares;
+	}
+
+	solveFirstQuadrant(squares){
+		let fillOptions = [1,2,3,4]
+		for(let i=0; i<4; i++){
+			squares[i].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+		}
+		return squares;
+	}
+
+	solveSecondQuadrant(squares){
+		let fillOptions = [1,2,3,4]
+		let removeIndex = fillOptions.indexOf(squares[0].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[1].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[4].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+		squares[5].value = fillOptions[0]
+
+		fillOptions = [1,2,3,4]
+		removeIndex = fillOptions.indexOf(squares[2].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[3].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[6].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+		squares[7].value = fillOptions[0]
+	}
+
+	solveThirdQuadrant(squares){
+		let fillOptions = [1,2,3,4]
+		let removeIndex = fillOptions.indexOf(squares[0].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[2].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[8].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+		squares[10].value = fillOptions[0]
+
+		fillOptions = [1,2,3,4]
+		removeIndex = fillOptions.indexOf(squares[1].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[3].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[9].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+		squares[11].value = fillOptions[0]
+	}
+
+	solveFourthQuadrant(squares){
+		let fillOptions = [1,2,3,4]
+		let removeIndex = fillOptions.indexOf(squares[4].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[6].value)
+		if(removeIndex!== -1){fillOptions.splice(removeIndex, 1)}
+		removeIndex = fillOptions.indexOf(squares[8].value)
+		if(removeIndex!== -1){fillOptions.splice(removeIndex, 1)}
+		removeIndex = fillOptions.indexOf(squares[9].value)
+		if(removeIndex!== -1){fillOptions.splice(removeIndex, 1)}
+
+		squares[12].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+
+		fillOptions = [1,2,3,4]
+		removeIndex = fillOptions.indexOf(squares[8].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[9].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[12].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[13].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+
+		fillOptions = [1,2,3,4]
+		removeIndex = fillOptions.indexOf(squares[4].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[6].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[12].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[14].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
+
+		fillOptions = [1,2,3,4]
+		removeIndex = fillOptions.indexOf(squares[5].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[7].value)
+		fillOptions.splice(removeIndex, 1)
+		removeIndex = fillOptions.indexOf(squares[13].value)
+		fillOptions.splice(removeIndex, 1)
+
+		squares[15].value = fillOptions.splice(Math.floor(Math.random() * fillOptions.length), 1)[0]
 	}
 
 	handleSquareClick(i) {
